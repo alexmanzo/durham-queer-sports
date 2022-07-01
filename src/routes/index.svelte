2bc976dc8ed9b2357bf0e6d27d59400fbeb998c9
Alex Manzo <manzoalex17@gmail.com>
2022-07-01 15:53:28 -0400

    Add supabase and update image handling.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,19 @@
 <script>
 	import * as animateScroll from 'svelte-scrollto';
-  import "@fontsource/fraunces/variable-full.css";
+	import '@fontsource/fraunces/variable-full.css';
+	import { supabase } from '$lib/supabaseClient';
+	let imgSrc;
+	let imgAlt;
+	async function downloadImage(node, imgId) {
+		try {
+			const { data, error } = await supabase.from('images').select('src, alt').eq('id', imgId);
+
+			imgSrc = data[0].src;
+			imgAlt = data[0].alt;
+		} catch (error) {
+			console.error('Error downloading image: ', error.message);
+		}
+	}
 </script>
 
 <header class="flex md:flex-row flex-col gap-4 md:gap-0 justify-between items-center py-5">
@@ -61,11 +74,7 @@
 				</p>
 			</div>
 			<div class="flex-1">
-				<img
-					class="rounded-2xl"
-					src="https://scontent-iad3-2.xx.fbcdn.net/v/t1.6435-9/71036419_10214649890945289_3604821770143531008_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=8631f5&_nc_ohc=lUwoSLgJ4bYAX9l9Vrl&_nc_ht=scontent-iad3-2.xx&oh=00_AT9uvK6_fVmyRMNCsxf0jSw7RE-gn4cm0EyvfIeKY3vTHQ&oe=62925228"
-					alt="group photo from folks playing kickball"
-				/>
+				<img class="rounded-2xl" src={imgSrc} alt={imgAlt} use:downloadImage={1} />
 			</div>
 		</div>
 	</section>
@@ -76,33 +85,45 @@
 				<p>Join Facebook groups for each of our leagues to stay up to date on upcoming events.</p>
 				<ul class="grid md:grid-cols-3 gap-4 mt-5">
 					<li>
-						<a class="border rounded-lg p-4 block hover:bg-slate-50 hover:underline" href="https://www.facebook.com/groups/DurhamQueerKickball" target="_blank"
-							>Kickball</a
+						<a
+							class="border rounded-lg p-4 block hover:bg-slate-50 hover:underline"
+							href="https://www.facebook.com/groups/DurhamQueerKickball"
+							target="_blank">Kickball</a
 						>
 					</li>
 					<li>
-						<a class="border rounded-lg p-4 block hover:bg-slate-50 hover:underline" href="https://www.facebook.com/groups/446538442593552" target="_blank"
-							>Soccer</a
+						<a
+							class="border rounded-lg p-4 block hover:bg-slate-50 hover:underline"
+							href="https://www.facebook.com/groups/446538442593552"
+							target="_blank">Soccer</a
 						>
 					</li>
 					<li>
-						<a class="border rounded-lg p-4 block hover:bg-slate-50 hover:underline" href="https://www.facebook.com/groups/DurmQueerBball/" target="_blank"
-							>Basketball</a
+						<a
+							class="border rounded-lg p-4 block hover:bg-slate-50 hover:underline"
+							href="https://www.facebook.com/groups/DurmQueerBball/"
+							target="_blank">Basketball</a
 						>
 					</li>
 					<li>
-						<a class="border rounded-lg p-4 block hover:bg-slate-50 hover:underline" href="https://www.facebook.com/groups/2198858146817694/" target="_blank"
-							>Ultimate</a
+						<a
+							class="border rounded-lg p-4 block hover:bg-slate-50 hover:underline"
+							href="https://www.facebook.com/groups/2198858146817694/"
+							target="_blank">Ultimate</a
 						>
 					</li>
 					<li>
-						<a class="border rounded-lg p-4 block hover:bg-slate-50 hover:underline" href="https://www.facebook.com/groups/2227800064187253/" target="_blank"
-							>Run club</a
+						<a
+							class="border rounded-lg p-4 block hover:bg-slate-50 hover:underline"
+							href="https://www.facebook.com/groups/2227800064187253/"
+							target="_blank">Run club</a
 						>
 					</li>
 					<li>
-						<a class="border rounded-lg p-4 block hover:bg-slate-50 hover:underline" href="https://www.facebook.com/groups/durhamqueertennis" target="_blank"
-							>Tennis</a
+						<a
+							class="border rounded-lg p-4 block hover:bg-slate-50 hover:underline"
+							href="https://www.facebook.com/groups/durhamqueertennis"
+							target="_blank">Tennis</a
 						>
 					</li>
 				</ul>
