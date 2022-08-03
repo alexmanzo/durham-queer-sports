6369c8c417069dccfdef71fdce64f306c3f84c1c
Alex Manzo <manzoalex17@gmail.com>
2022-08-03 09:53:35 -0400

    Add honeypoint to login form.

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,37 +1,42 @@
 <script>
-  import {supabase} from "$lib/supabaseClient"
+	import { supabase } from '$lib/supabaseClient';
 
-  let loading = false
-  let email;
+	let loading = false;
+	let email;
+	let honeypotField;
 
-  const handleLogin = async () => {
-    try {
-      loading = true
-      const { error } = await supabase.auth.signIn({ email })
-      if (error) throw error
-      alert('Check your email for the login link!')
-    } catch (error) {
-      alert(error.error_description || error.message)
-    } finally {
-      loading = false
-    }
-  }
+	const handleLogin = async () => {
+		if (!honeypotField) {
+			try {
+				loading = true;
+				const { error } = await supabase.auth.signIn({ email });
+				if (error) throw error;
+				alert('Check your email for the login link!');
+			} catch (error) {
+				alert(error.error_description || error.message);
+			} finally {
+				loading = false;
+			}
+		}
+	};
 </script>
 
 <form class="row flex flex-center" on:submit|preventDefault={handleLogin}>
-  <div class="col-6 form-widget">
-    <h1 class="header">Supabase + Svelte</h1>
-    <p class="description">Sign in via magic link with your email below</p>
-    <div>
-      <input
-        class="inputField"
-        type="email"
-        placeholder="Your email"
-        bind:value={email}
-      />
-    </div>
-    <div>
-      <input type="submit" class='button block' value={loading ? "Loading" : "Send magic link"} disabled={loading} />
-    </div>
-  </div>
+	<div class="col-6 form-widget">
+		<h1 class="header">Supabase + Svelte</h1>
+		<p class="description">Sign in via magic link with your email below</p>
+		<div>
+			<input class="inputField" type="email" placeholder="Your email" bind:value={email} />
+		</div>
+		<label for="petName" class="hidden">Pet Name</label>
+		<input type="hidden" name="petName" id="petName" bind:value={honeypotField} />
+		<div>
+			<input
+				type="submit"
+				class="button block"
+				value={loading ? 'Loading' : 'Send magic link'}
+				disabled={loading}
+			/>
+		</div>
+	</div>
 </form>
