e124f5a0f6cf243501212eb4145345792bb696cd
Alex Manzo <manzoalex17@gmail.com>
2022-08-26 17:12:00 -0400

    Add schedule page.

---
+++ src/lib/components/ScheduleItem.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let dayOfWeek;
+	export let location;
+	export let eventName;
+</script>
+
+<li class="border rounded-lg p-4 block bg-indigo-12 text-indigo-1">
+	<h2 class="font-serif font-medium text-center text-xl tracking-wider ">{dayOfWeek}</h2>
+	<div class="flex flex-col gap-1 justify-center items-center mt-3">
+		<p class="text-xl">{eventName}</p>
+		<p>6:00 PM</p>
+		<p>{location}</p>
+	</div>
+</li>
---
+++ src/routes/schedule.svelte
@@ -0,0 +1,16 @@
+<script>
+import Header from "$lib/components/Header.svelte";
+import ScheduleItem from "$lib/components/ScheduleItem.svelte";
+</script>
+
+<Header />
+<section>
+	<h1 class="font-serif font-normal text-center text-xl lg:text-4xl">Schedule</h1>
+	<ul class="grid md:grid-cols-2 gap-4 mt-7">
+    <ScheduleItem dayOfWeek="Monday" location="George Watts Elementary" eventName="Soccer" />
+    <ScheduleItem dayOfWeek="Tuesday" location="Hope Valley Farms YMCA" eventName="Tennis" />
+    <ScheduleItem dayOfWeek="Wednesday" location="Forest Hills Park" eventName="Kickball" />
+    <ScheduleItem dayOfWeek="Thursday" location="Hope Valley Farms YMCA" eventName="Tennis" />
+    <ScheduleItem dayOfWeek="Sunday" location="Forest Hills Park (Sculpture Field)" eventName="Ultimate" />
+  </ul>
+</section>
