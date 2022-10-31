7c7dd2e263c9969454200a5fa901c01c65488700
Alex Manzo <35744517+alexmanzo@users.noreply.github.com>
2022-10-31 09:36:23 -0400

    Update ScheduleItem component to take time prop.

--- src/lib/components/ScheduleItem.svelte
+++ src/lib/components/ScheduleItem.svelte
@@ -2,13 +2,14 @@
 	export let dayOfWeek;
 	export let location;
 	export let eventName;
+	export let eventTime;
 </script>
 
 <li class="border rounded-lg p-4 block bg-indigo-12 text-indigo-1">
 	<h2 class="font-serif font-medium text-center text-xl tracking-wider ">{dayOfWeek}</h2>
 	<div class="flex flex-col gap-1 justify-center items-center mt-3">
 		<p class="text-xl">{eventName}</p>
-		<p>6:00 PM</p>
+		<p>{eventTime}</p>
 		<p>{location}</p>
 	</div>
 </li>
