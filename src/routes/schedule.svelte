431c86fd2516cf55daa305f9218214e5d106c67e
Alex Manzo <35744517+alexmanzo@users.noreply.github.com>
2022-10-31 09:40:30 -0400

    Update schedule.svelte

--- src/routes/schedule.svelte
+++ src/routes/schedule.svelte
@@ -4,12 +4,12 @@ import ScheduleItem from "$lib/components/ScheduleItem.svelte";
 </script>
 
 <section>
-	<h1 class="font-serif font-normal text-center text-xl lg:text-4xl">Schedule</h1>
+        <h1 class="font-serif font-normal text-center text-xl lg:text-4xl">Schedule</h1>
 	<ul class="grid md:grid-cols-2 gap-4 mt-7">
-    <ScheduleItem dayOfWeek="Monday" location="George Watts Elementary" eventName="Soccer" />
-    <ScheduleItem dayOfWeek="Tuesday" location="Hope Valley Farms YMCA" eventName="Tennis" />
-    <ScheduleItem dayOfWeek="Wednesday" location="Forest Hills Park" eventName="Kickball" />
-    <ScheduleItem dayOfWeek="Thursday" location="Hope Valley Farms YMCA" eventName="Tennis" />
-    <ScheduleItem dayOfWeek="Sunday" location="Forest Hills Park (Sculpture Field)" eventName="Ultimate" />
+                <ScheduleItem dayOfWeek="Monday" location="Forest Hills Park (Sculpture Field)" eventName="Soccer" eventTime="5:30PM" />
+                <ScheduleItem dayOfWeek="Tuesday" location="Hope Valley Farms YMCA" eventName="Tennis" eventTime="5:30PM" />
+                <ScheduleItem dayOfWeek="Thursday" location="Hope Valley Farms YMCA" eventName="Tennis" eventTime="5:30PM" />
+                <ScheduleItem dayOfWeek="Sunday" location="Forest Hills Park (Sculpture Field)" eventName="Ultimate" eventTime="2:00PM" />
+                <ScheduleItem dayOfWeek="Sunday" location="Forest Hills Park" eventName="Kickball" eventTime="3:30PM" />
   </ul>
 </section>
