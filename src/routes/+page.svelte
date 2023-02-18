18c8c86fd55eb89ca8fb3ebfd0dec5700ff43b0d
Alex Manzo <manzoalex17@gmail.com>
2023-02-18 09:34:23 -0500

    lay out rest of homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,10 @@
 	import '@fontsource/open-sans/400.css';
 
 	import Hero from '$lib/components/Hero.svelte';
-	// import About from '$lib/components/About.svelte';
-	// import Leagues from '$lib/components/Leagues.svelte';
+	import About from '$lib/components/About.svelte';
+	import Leagues from '$lib/components/Leagues.svelte';
 </script>
 
 <Hero />
-<!-- <About />
-<Leagues /> -->
+<About />
+<Leagues />
