f7124b37a79ac3aef0329920e1eb99d208b4fcc3
Alex Manzo <manzoalex17@gmail.com>
2023-02-19 11:46:31 -0500

    add twilio.

--- functions/alert-webhook/alert-webhook.js
+++ functions/alert-webhook/alert-webhook.js
@@ -1,6 +1,17 @@
+import twilio from 'twilio';
+
+const accountSid = 'AC5294cd07d14e574b94e1c0a1bd574732';
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
+
 export const handler = async (event, context) => {
 	try {
-		console.log(event.body);
+		const alertContent = JSON.parse(event.body.data);
+		const { title, content } = alertContent;
+
+		client.messages
+			.create({ body: `${title}: ${content}`, from: '+18557823971', to: '+19844848933' })
+			.then((message) => console.log(message.sid));
 		return {
 			statusCode: 200
 		};
