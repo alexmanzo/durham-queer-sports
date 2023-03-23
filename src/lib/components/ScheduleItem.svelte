3bcf4d8be985c16200b7692f2206384ed6f72251
Alex Manzo <manzoalex17@gmail.com>
2023-03-23 12:16:59 -0400

    Add support for social/one-off events.

--- src/lib/components/ScheduleItem.svelte
+++ src/lib/components/ScheduleItem.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <li class="border rounded-lg p-4 block bg-indigo-12 text-indigo-1">
-	<h2 class="font-serif font-medium text-center text-xl tracking-wider ">{dayOfWeek}</h2>
+	<h3 class="font-serif font-medium text-center text-xl tracking-wider ">{dayOfWeek}</h3>
 	<div class="flex flex-col gap-1 justify-center items-center mt-3">
 		<p class="text-xl">{eventName}</p>
 		<p>{eventTime}</p>
