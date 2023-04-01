cd5670d524d6162ff75f9d972d346a6ed1d15344
Alex Manzo <manzoalex17@gmail.com>
2023-04-01 17:01:34 -0400

    fix svg in hero.

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -10,11 +10,11 @@
 </script>
 
 <section
-	class="flex flex-col items-center justify-between md:flex-row-reverse lg:my-24 md:my-20 my-10"
+	class="flex flex-col items-center justify-between gap-7 lg:flex-row-reverse lg:my-24 md:my-20 my-10"
 >
 	<h1 class="sr-only">Durham Queer Sports</h1>
 	<div class="flex-1 leading-normal text-3xl">
-		<p class="text-center md:text-left">
+		<p class="text-center lg:text-left">
 			At Durham Queer Sports, our mission is to create a <strong
 				>free, empowering, social, and athletic</strong
 			>
@@ -23,7 +23,7 @@
 	</div>
 	<div class="flex-1">
 		{#if LottiePlayer}
-			<div class="mt-8 md:mt-0 max-w-full md:max-w-lg">
+			<div class="mt-8 md:mt-0 max-w-full md:max-w-lg relative -z-10">
 				<LottiePlayer
 					src="https://assets3.lottiefiles.com/private_files/lf30_fmcseocu.json"
 					mode="bounce"
