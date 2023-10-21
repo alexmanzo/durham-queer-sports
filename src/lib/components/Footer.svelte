20b4f730cfeb144563ab5012156be0ec09963f3c
Alex Manzo <manzoalex17@gmail.com>
2023-10-21 08:54:15 -0400

    Add donate link to footer.

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,12 +1,19 @@
 <footer class="bg-indigo py-14 text-white mx-full border-t-indigo-10 border-t-8 relative">
 	<div class="border-y-8 border-t-indigo-9 border-b-indigo-8 absolute top-0 w-full" />
 	<div class="border-y-8 border-t-indigo-7 border-b-indigo-6 absolute top-0 w-full" />
-	<div class="container mx-auto px-3">
+	<div class="container mx-auto px-3 flex flex-col items-center">
 		<h2 class="text-3xl mb-5 font-serif font-medium tracking-wide text-center">
-			Sign up for our newsletter
+			Support Durham Queer Sports
 		</h2>
+		<a
+			href="https://www.paypal.com/paypalme/durhamqueersports?country.x=US&locale.x=en_US"
+			target="_blank"
+			rel="noreferrer"
+			class="bg-indigo-12 hover:bg-opacity-80 focus-visible:outline focus-visible:outline-indigo-11 focus-visible:outline-2 outline-offset-4 transition-all duration-100 text-indigo-1 font-medium tracking-wide px-14 py-3 rounded text-lg"
+			>Donate
+		</a>
 		<!-- Begin Mailchimp Signup Form -->
-		<div id="mc_embed_signup">
+		<!-- <div id="mc_embed_signup">
 			<form
 				action="https://gmail.us19.list-manage.com/subscribe/post?u=d6288a249cf5353687fd8ee88&amp;id=9bb8fb8306&amp;f_id=0046ace4f0"
 				method="post"
@@ -34,7 +41,6 @@
 					<div class="response" id="mce-error-response" style="display:none" />
 					<div class="response" id="mce-success-response" style="display:none" />
 				</div>
-				<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 				<div style="position: absolute; left: -5000px;" aria-hidden="true">
 					<input type="text" name="b_d6288a249cf5353687fd8ee88_9bb8fb8306" tabindex="-1" value="" />
 				</div>
@@ -44,7 +50,7 @@
 					>Sign me up!</button
 				>
 			</form>
-		</div>
+		</div> -->
 		<!--End mc_embed_signup-->
 	</div>
 </footer>
